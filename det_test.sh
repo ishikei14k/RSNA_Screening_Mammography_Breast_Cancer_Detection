a4a13367004b6c1cfc1ed8303fd8cde718d6d513
ishi_keiichirou.Rist <keiichirou.ishi@rist.co.jp>
2023-02-25 23:33:45 +0900

    remove unnecessary cmd from `det_test.sh`

--- det_test.sh
+++ det_test.sh
@@ -5,11 +5,6 @@ python tools/det/test.py data/work_dirs/det/${exp}/${exp}.py \
     data/work_dirs/det/${exp}/latest.pth \
     --out data/work_dirs/det/${exp}/det_result.pkl
 
-python tools/det/make_crops.py det_ann/train.json \
-    data/work_dirs/det/${exp}/det_result.pkl \
-    data/preprocessed/image_resized_2048V \
-    data/work_dirs/det/${exp}/cropped_images
-
 python tools/det/postprocess/result_to_csv.py \
     det_ann/train.json \
     data/work_dirs/det/${exp}/det_result.pkl \
